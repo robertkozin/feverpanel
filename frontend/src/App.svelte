9ef87b0faf45f190b0e6ed0c400c09220b8b508f
nathanielfernandes <nathaniel.s.fernandes@gmail.com>
2025-12-26 15:29:40 -0500

    start off frontend

---
+++ frontend/src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+</script>
+
+<main class="min-h-screen flex flex-col bg-base-200">
+  <div class="navbar bg-base-100"> 
+    <a class="font-bold text-xl mx-3 hover:underline" href="/">feverpanel</a>
+  </div>
+
+  <div class="flex-1">
+    <div class="container mx-auto px-4 py-8">
+      Hello world!
+    </div>
+  </div>
+</main>
